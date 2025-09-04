4e6bcfb7216b5283940d5f6198572389e161dca8
Robert Fancher <robert@ideasandpixels.com>
2025-09-03 21:43:44 -0400

    Prepare Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// GitHub Pages project site: base must be '/<repo-name>/'
 export default defineConfig({
   plugins: [vue()],
+  base: '/fanchstudios/',
 })
